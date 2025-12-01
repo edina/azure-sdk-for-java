109e4e92e84815e1131c5a5bac9a34d4f9756b85
Andrew Seales <andrew.seales@ed.ac.uk>
2025-12-01 14:16:53 +0000

    Bump version to beta.11-edina

--- sdk/openai/azure-ai-openai/deploy-hack.sh
+++ sdk/openai/azure-ai-openai/deploy-hack.sh
@@ -1,10 +1,11 @@
+version=1.0.0-beta.11-edina
 mvn deploy:deploy-file \
     -DskipTests \
-    -Dfile=target/azure-ai-openai-1.0.0-beta.10-edina.jar \
+    -Dfile=target/azure-ai-openai-${version}.jar \
     -DrepositoryId=edina-repository \
     -Durl=scpexe://geodev.edina.ac.uk/var/www/html/maven-repository \
     -DartifactId=azure-ai-openai \
     -DgroupId=com.azure \
     -DpomFile=pom.xml \
-    -Dsources=target/azure-ai-openai-1.0.0-beta.10-edina-sources.jar \
-    -Dversion=1.0.0-beta.10-edina
+    -Dsources=target/azure-ai-openai-${version}-sources.jar \
+    -Dversion=${version}
